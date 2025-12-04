47b1446e59decd884fb80da030d65339a64bf3e4
Dharunkumar <dharunkumar.n2024cse@sece.ac.in>
2025-12-04 10:34:31 +0530

    JS LEARN

--- js/index.js
+++ js/index.js
@@ -1,2 +1,36 @@
 var a =10;
 console.log(a);
+var b=20;
+console.log(b);
+
+function named()
+{
+    console.log("This is a named function");
+    console.log("This is a named function");
+    console.log("This is a named function");
+}
+
+named();
+let functionExpression = function()
+{
+    console.log("This is a function expression");
+    console.log("This is a function expression");
+    console.log("This is a function expression");
+}
+functionExpression();
+
+let arrow =()=>
+{
+    console.log("This is an arrow function");
+    console.log("This is an arrow function");
+    console.log("This is an arrow function");
+}
+arrow();
+
+//call back function 
+(()=>
+{
+    console.log("This is a call back function");
+    console.log("This is a call back function");
+    console.log("This is a call back function");
+})();
